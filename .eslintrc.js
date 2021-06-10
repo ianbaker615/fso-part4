e2ded36836af05f846035ad92d1efdcf4e604691
Ian Baker <ianbaker615@gmail.com>
2021-06-10 16:18:34 -0400

    4.1-4.3 unit testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
   extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
   },
   rules: {
     indent: ["error", 2],
---
+++ utils/list_helper.js
@@ -0,0 +1,8 @@
+const dummy = (blogs) => {
+  // 4.3
+  return 1;
+};
+
+module.exports = {
+  dummy,
+};
